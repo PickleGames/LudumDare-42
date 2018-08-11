9cc50bf5b2897063a04cfbe71347320543b7f471
Howarddful123 <Howarddful123@gmail.com>
2018-08-11 16:46:33 -0700

    -Added new Train Sprites. -Added Train Mechanic Script(Still testing).

---
+++ LudumDare42/Assets/Scripts/TrainMechanic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TrainMechanic : MonoBehaviour {
+
+
+
+
+
+    //FOR TESTING ONLY
+    void OnMouseOver()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        Debug.Log("SEE IN");
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+        Debug.Log("Stop Seeing Inside");
+    }
+}
