41f1339d0c4e144d046209b89fe01f6b1cebc296
Miguel Garnica <mgarnica123@aol.com>
2018-08-11 23:34:47 -0700

    GameConductor
    control when train is traveling and when train is at station

--- LudumDare42/Assets/Scripts/GameConductor.cs
+++ LudumDare42/Assets/Scripts/GameConductor.cs
@@ -4,32 +4,59 @@ using UnityEngine;
 
 public class GameConductor : MonoBehaviour {
 
-    public GameObject bg, fg;
+    public GameObject parallax;
+    public GameObject tunnel;
+    public GameObject trainStation;
 
     public float travelTime;
-    public bool atStation;
-
+    public float stationTime;
+    private bool atStation;
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-    float tTimer;
+    float tTimer, stimer;
 	// Update is called once per frame
 	void Update () {
-        if (!atStation)
+     
+        if (atStation)
+        {
+            stimer += Time.deltaTime;
+            if(stimer > stationTime)
+            {
+                tunnel.GetComponent<Tunnel>().ResetTunnel();
+                stimer = 0;            
+                atStation = false;
+            }
+        }
+        else
         {
             tTimer += Time.deltaTime;
             if (tTimer > travelTime)
             {
+                tunnel.GetComponent<Tunnel>().ResetTunnel();
                 tTimer = 0;
                 atStation = true;
+               
             }
         }
 
-        if (atStation)
+
+        if (tunnel.GetComponent<Tunnel>().InCenter())
         {
-            bg.GetComponentInChildren<Tiling>().station = true;
+            if (atStation)
+            {
+                parallax.SetActive(false);
+                trainStation.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("TURN ON PARALLAX");
+                parallax.SetActive(true);
+                trainStation.SetActive(false);
+            }
         }
 	}
+
 }
