f921786fe3ea354cc46d7877eda94c36bc045513
Nguyen Duong <ducnguyenduchao@gmail.com>
2018-08-13 15:39:02 -0700

    Added Money display for ai

--- LudumDare42/Assets/Scripts/Player/AI.cs
+++ LudumDare42/Assets/Scripts/Player/AI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AI : MonoBehaviour {
@@ -15,23 +16,67 @@ public class AI : MonoBehaviour {
     private float timeElapsedColor;
     private bool isColorChanged;
 
+    private TextMeshPro textMoney;
+    private float timeMoney;
+    private float timeMoneyDis;
+    private bool isMoneyEnable;
+    private SpriteRenderer sprite;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        SpriteRenderer sr = GetComponentInChildren<SpriteRenderer>();
-        sr.enabled = false;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.enabled = false;
         health = 3;
+        textMoney = this.GetComponentInChildren<TextMeshPro>();
+        textMoney.enabled = false;
     }
 
+    public void EnableSpeech()
+    {
+        isMoneyEnable = true;
+        textMoney.enabled = true;
+    }
 
+    public void DisableSpeech()
+    {
+        textMoney.enabled = false;
+    }
+
+    float delay = 2;
     void Update()
     {
+        if (sprite.enabled)
+        {
+            timeMoney += Time.deltaTime;
+            if (timeMoney > delay)
+            {
+                timeMoney = 0;
+                EnableSpeech();
+                delay = Random.Range(1f, 3f);
+            }
+            if (isMoneyEnable)
+            {
+                timeMoneyDis += Time.deltaTime;
+                if (timeMoneyDis >= 2)
+                {
+                    isMoneyEnable = false;
+                    timeMoneyDis = 0;
+                    DisableSpeech();
+                }
+            }
+        }else
+        {
+            DisableSpeech();
+        }
+        
+
+
         if (IsFly)
         {
             transform.Translate(new Vector2(-.35f, 0));
-            transform.RotateAround(transform.position, Vector3.up, 5);
+            transform.RotateAround(transform.position, Vector3.down, -5);
         }
         if(health <= 0)
         {
@@ -68,6 +113,8 @@ public class AI : MonoBehaviour {
         trainCart.RemovePeople(this.gameObject);
         AudioSource scream = this.GetComponent<AudioSource>();
         scream.clip = clips[0];
+        scream.volume = 0.25f;
+        scream.pitch = Random.Range(.65f, 1f);
         if (!scream.isPlaying)
         {
             scream.Play();
