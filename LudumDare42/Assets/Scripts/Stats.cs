6e439d1b89a575761d58691112fe2b6cf7df7352
Miguel Garnica <mgarnica123@aol.com>
2018-08-13 13:22:16 -0700

    Stats
    Keep track of stats

---
+++ LudumDare42/Assets/Scripts/Stats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Stats : MonoBehaviour {
+    public TextMeshProUGUI[] statTexts;
+    public string[] statNames;
+    public float[] statNums;
+    public static Stats instance;
+	// Use this for initialization
+	void Start () {
+        instance = this;
+        DontDestroyOnLoad(this);
+		for(int i = 0; i < statTexts.Length; i++)
+        {
+            statTexts[i].text = statNames[i] + ": " + statNums;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < statTexts.Length; i++)
+        {
+            statTexts[i].text = statNames[i] + ": " + statNums;
+        }
+    }
+}
