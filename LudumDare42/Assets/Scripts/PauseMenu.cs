bb15b35ac49642953db31f2da00ebd6dcc9d7d83
Howarddful123 <Howarddful123@gmail.com>
2018-08-29 15:39:55 -0700

    -Updated Hao Scene. -Added PauseUI sprite. -Added PauseMenu Script.

---
+++ LudumDare42/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public static bool isPaused = false;
+
+    public GameObject pauseUI;
+	void Update () {
+        if (Input.GetKeyDown(Keycode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    void Resume ()
+    {
+
+    }
+}
