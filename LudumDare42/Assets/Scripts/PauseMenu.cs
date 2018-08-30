a6f5b9d5f5d38235de25a09ef3cfe31ad666fa24
Howarddful123 <Howarddful123@gmail.com>
2018-08-29 17:35:59 -0700

    -Improved Pause Menu. -You can now click outside the pause panel to unpause.

--- LudumDare42/Assets/Scripts/PauseMenu.cs
+++ LudumDare42/Assets/Scripts/PauseMenu.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
     public static bool isPaused = false;
+    public GameObject backgroundButton;
 
+    public GameObject backgroundStop;
+    public GameObject foreGroundStop;
+    public GameObject shadowGroundStop;
     public GameObject pauseUI;
-	void Update () {
-        if (Input.GetKeyDown(Keycode.Escape))
+
+    void Start()
+    {
+
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
             {
@@ -20,8 +31,60 @@ public class PauseMenu : MonoBehaviour {
         }
 	}
 
-    void Resume ()
+    private void Resume ()
     {
+        foreach (Transform child in backgroundStop.transform)
+        {
+            child.GetComponent<Tiling>().moveSpeed = 0.2f;
+        }
+        foreach (Transform child in foreGroundStop.transform)
+        {
+            child.GetComponent<Tiling>().moveSpeed = 0.5f;
+        }
+        foreach (Transform child in shadowGroundStop.transform)
+        {
+            child.GetComponent<Tiling>().moveSpeed = 0.5f;
+        }
+        //backgroundStop.GetComponent<Tiling>().moveSpeed = 0.2f;
+        //foreGroundStop.GetComponent<Tiling>().moveSpeed = 0.5f;
+        //shadowGroundStop.GetComponent<Tiling>().moveSpeed = 0.5f;
+        pauseUI.SetActive(false);
+        backgroundButton.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
 
+    }
+
+    private void Pause()
+    {
+        foreach(Transform child in backgroundStop.transform)
+        {
+            child.GetComponent<Tiling>().moveSpeed = 0f;
+        }
+        foreach (Transform child in foreGroundStop.transform)
+        {
+            child.GetComponent<Tiling>().moveSpeed = 0f;
+        }
+        foreach (Transform child in shadowGroundStop.transform)
+        {
+            child.GetComponent<Tiling>().moveSpeed = 0f;
+        }
+
+        backgroundButton.SetActive(true);
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void PauseButton()
+    {
+        if (isPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
     }
 }
